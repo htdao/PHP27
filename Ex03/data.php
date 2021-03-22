4047a59be4f8c0b6ef4a74364253e4d341bdbb2b
htdao <htdao00@gmail.com>
2021-03-22 16:48:04 +0700

    Ex03

---
+++ Ex03/data.php
@@ -0,0 +1,8 @@
+<?php 
+	$data = array();
+	$data[0]['name'] = 'Toán rời rạc';
+	$data[0]['file_name'] = 'file_name1.doc';
+	$data[1]['name'] = 'Cấu trúc dữ liệu và giải thuật';
+	$data[1]['file_name'] = 'file_name2.doc';
+
+ ?>
---
+++ Ex03/remove.php
@@ -0,0 +1,4 @@
+<?php 
+	session_start();
+	unset($_SESSION['file']['file_name']);
+ ?>
